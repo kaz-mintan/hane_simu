ae87a75769b9515be92486adfa55809dab68931d
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-09-03 19:51:47 +0900

    modified to be as class

--- dummy_modules/hand_motion.py
+++ dummy_modules/hand_motion.py
@@ -1,32 +1,41 @@
 # coding:utf-8
-
 import numpy as np
+from matplotlib import pyplot as plt
 
-def assumed_z(present_z, limit_max, limit_min):
-    array_size = 1
-    assumed_dz = np.random.uniform(low=-0.01,high=0.01,size=array_size)
-    assumed_z = assumed_dz + present_z
-    if assumed_z > limit_max:
-        assumed_z = assumed_z - assumed_dz
-    elif assumed_z < limit_min:
-        assumed_z = assumed_z - assumed_dz
+class dummy_Hand:
+    def __init__(self, init_z):
+        self.present_z=init_z
+        self.array_size = 1
 
-    return assumed_z
+    def simu_z(self, limit_max, limit_min):
+        assumed_dz = np.random.uniform(low=-0.01,high=0.01,size=self.array_size)
+        assumed_z = assumed_dz + self.present_z
+        if assumed_z > limit_max:
+            assumed_z = assumed_z - assumed_dz
+        elif assumed_z < limit_min:
+            assumed_z = assumed_z - assumed_dz
 
-def get_ir(present_z):
-    limit_max=1
-    limit_min=0
-    z = assumed_z(present_z,limit_max,limit_min)
-    return z
+        return assumed_z
+
+    def get_ir(self):
+        limit_max=1
+        limit_min=0
+        z = self.simu_z(limit_max,limit_min)
+        return z
 
 if __name__ == '__main__':
-    loop_val = 100
+    #python hand_motion.py <with no argument>
     z_depth = 0
+    hand_simu=dummy_Hand(z_depth)
+    loop_val = 100
     zlimit_max = 1
     zlimit_min = 0
     z=np.zeros(loop_val)
     for i in range(loop_val):
         z[i]=z_depth
-        z_depth = assumed_z(z_depth,zlimit_max,zlimit_min)
+        z_depth = hand_simu.simu_z(zlimit_max,zlimit_min)
+    plt.plot(z)
+    plt.show()
+    print("end")
 
-    np.savetxt('test_trajectory.csv',z,delimiter=',')
+    #np.savetxt('test_trajectory.csv',z,delimiter=',')
